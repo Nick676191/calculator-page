b5eac6d55631ed6b905d10f50b876af03560c1c1
Nick Bourgeois <77581387+Nick676191@users.noreply.github.com>
2025-11-21 22:47:13 -0600

    Defining the 10 numbered buttons for the calculator

--- script.js
+++ script.js
@@ -23,3 +23,15 @@ let secondNum;
 function operate(operator, numOne, numTwo) {
     return operator(numOne, numTwo);
 };
+
+// creating all of the buttons inside of the calculator div
+const calculator = document.querySelector(".calc-container");
+for (let i = 0; i < 10; i++) {
+    let btn = document.createElement("button");
+    btn.classList.add("num");
+    btn.textContent = i;
+    // grabs the width of the calculator div and divides it by 3
+    btn.style.width = calculator.clientWidth / 3 + "px";
+    btn.style.height = btn.style.width;
+    calculator.appendChild(btn);
+};
