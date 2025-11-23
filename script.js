f6ab5895120089826c961f172dd13533f2a7d751
Nick Bourgeois <77581387+Nick676191@users.noreply.github.com>
2025-11-22 23:18:28 -0600

    Transfer over javascript DOM manipulation into HTML code to make the placement of javascript functions easier

--- script.js
+++ script.js
@@ -27,28 +27,28 @@ function operate(operator, numOne, numTwo) {
 // creating all of the buttons inside of the calculator div
 const calculator = document.querySelector(".calc-container");
 
-const calcWindow = document.createElement("div");
-calcWindow.classList.add("window");
-calculator.appendChild(calcWindow);
+// const calcWindow = document.createElement("div"); /
+// calcWindow.classList.add("window");
+// calculator.appendChild(calcWindow);
 
-const numField = document.createElement("h1");
-numField.classList.add("show");
-numField.textContent = "NUM"
-calcWindow.appendChild(numField);
+// const numField = document.createElement("h1"); /
+// numField.classList.add("show");
+// // numField.textContent = popDisplay();
+// calcWindow.appendChild(numField);
 
-const btnContainer = document.createElement("div");
-btnContainer.classList.add("btns");
-calculator.appendChild(btnContainer);
+// const btnContainer = document.createElement("div"); /
+// btnContainer.classList.add("btns");
+// calculator.appendChild(btnContainer);
 
-const numContainer = document.createElement("div");
-numContainer.classList.add("numbers");
+// const numContainer = document.createElement("div"); /
+// numContainer.classList.add("numbers");
 const numContainerWidth = btnContainer.clientWidth * (3/4);
 numContainer.style.width = numContainerWidth + "px";
-btnContainer.appendChild(numContainer);
+// btnContainer.appendChild(numContainer);
 
-const operatorContainer = document.createElement("div");
-operatorContainer.classList.add("operators");
-btnContainer.appendChild(operatorContainer);
+// const operatorContainer = document.createElement("div"); /
+// operatorContainer.classList.add("operators");
+// btnContainer.appendChild(operatorContainer);
 
 for (let i = 0; i < 10; i++) {
     let btn = document.createElement("button");
@@ -63,55 +63,66 @@ for (let i = 0; i < 10; i++) {
     numContainer.appendChild(btn);
 };
 
-const clearButton = document.createElement("button");
-const firstBut = document.querySelector(".num");
-clearButton.classList.add("clear");
+// const clearButton = document.createElement("button"); /
+// const firstBut = document.querySelector(".num");
+// clearButton.classList.add("clear");
 clearButton.style.width = numContainer.clientWidth + "px";
 clearButton.style.height = firstBut.style.height;
-clearButton.textContent = "clear";
-numContainer.insertBefore(clearButton, firstBut);
+// clearButton.textContent = "clear";
+// numContainer.insertBefore(clearButton, firstBut);
 
-const decBtn = document.createElement("button");
-decBtn.classList.add("decimal");
+// const decBtn = document.createElement("button"); /
+// decBtn.classList.add("decimal");
 decBtn.style.width = firstBut.style.width;
 decBtn.style.height = firstBut.style.height;
-decBtn.textContent = ".";
-numContainer.appendChild(decBtn);
+// decBtn.textContent = ".";
+// numContainer.appendChild(decBtn);
 
-const posNegButton = document.createElement("button");
-posNegButton.classList.add("pos-neg");
+// const posNegButton = document.createElement("button"); /
+// posNegButton.classList.add("pos-neg");
 posNegButton.style.width = firstBut.style.width;
 posNegButton.style.height = firstBut.style.height;
-posNegButton.textContent = "+/-"
-numContainer.appendChild(posNegButton);
+// posNegButton.textContent = "+/-"
+// numContainer.appendChild(posNegButton);
 
 // creating the buttons in the operator container
-const eqBtn = document.createElement("button");
-eqBtn.classList.add("op");
-eqBtn.id = "activate";
-eqBtn.textContent = "=";
-operatorContainer.appendChild(eqBtn);
+// const eqBtn = document.createElement("button"); /
+// eqBtn.classList.add("op");
+// eqBtn.id = "activate";
+// eqBtn.textContent = "=";
+// operatorContainer.appendChild(eqBtn);
 
-const plusBtn = document.createElement("button");
-plusBtn.classList.add("op");
-plusBtn.id = "oper";
-plusBtn.textContent = "+";
-operatorContainer.appendChild(plusBtn);
+// const plusBtn = document.createElement("button"); /
+// plusBtn.classList.add("op");
+// plusBtn.id = "oper";
+// plusBtn.textContent = "+";
+// operatorContainer.appendChild(plusBtn);
 
-const minusBtn = document.createElement("button");
-minusBtn.classList.add("op");
-minusBtn.id = "oper";
-minusBtn.textContent = "-";
-operatorContainer.appendChild(minusBtn);
+// const minusBtn = document.createElement("button"); /
+// minusBtn.classList.add("op");
+// minusBtn.id = "oper";
+// minusBtn.textContent = "-";
+// operatorContainer.appendChild(minusBtn);
 
-const multBtn = document.createElement("button");
-multBtn.classList.add("op");
-multBtn.id = "oper";
-multBtn.textContent = "*";
-operatorContainer.appendChild(multBtn);
+// const multBtn = document.createElement("button"); /
+// multBtn.classList.add("op");
+// multBtn.id = "oper";
+// multBtn.textContent = "*";
+// operatorContainer.appendChild(multBtn);
 
-const divideBtn = document.createElement("button");
-divideBtn.classList.add("op");
-divideBtn.id = "oper";
-divideBtn.textContent = "/";
-operatorContainer.appendChild(divideBtn);
+// const divideBtn = document.createElement("button"); /
+// divideBtn.classList.add("op");
+// divideBtn.id = "oper";
+// divideBtn.textContent = "/";
+// operatorContainer.appendChild(divideBtn);
+
+// creating functions to populate the display once a digit has been pressed
+function popDisplay() {
+    const digBtns = numContainer.querySelectorAll("#btn");
+    digBtns.forEach(digBtn => digBtn.addEventListener("click", (digBtn) => {
+        return digBtn.textContent;
+    }));
+};
+
+// prob going to have to comment out a lot of this javascript code and transfer it over to HTML
+// the ordering of the divs that are created in js is messing with some of the functions
